a3b40472268d6a92232248c86858f7a1eb812d52
aCosmicDebugger <isaac.projetos.123@gmail.com>
2023-08-01 20:50:03 -0300

    Adicionado o Hello-ApiVew

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Tes API View."""
+
+    def get(self, resquest, format=None):
+        """ Retorna uma lista das recursos do APIView."""
+
+        o_apiview = [
+                'Usa métodos HTTP como funções(get, post, patch, put, delete)',
+                'Te dá maior controle sobre a lógica da sua aplicação',
+                'É mapeada manualmente para URLs'
+        ]
+
+        return Response({'message':'Hello!', 'an_apiview': an_apiview})
