8de76341d452c71a8e9fccfe9e79f293982340d9
aCosmicDebugger <isaac.projetos.123@gmail.com>
2023-08-04 23:46:33 -0300

    Adicionado o Login na API

--- profile_api/views.py
+++ profile_api/views.py
@@ -4,7 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profile_api import serializers
 from profile_api import models
@@ -104,3 +105,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """ Lida com a criação de tokens da autenticação
+    """
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
